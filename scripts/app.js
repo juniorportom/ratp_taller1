a9e9dcc460041da80489868baf16dd6c0a3ecd42
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-01-31 19:53:31 -0500

    (F3) Se crea y adiciona codigo, para almacenamiento de app shell en cache

--- scripts/app.js
+++ scripts/app.js
@@ -200,4 +200,13 @@
         ];
         app.saveSelectedTimetables();
     }
+
+    // Agregar codigo de service worker
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function() { console.log('Service Worker Registered'); });
+    }
+
 })();
